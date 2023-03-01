e5904d2bc7f3e452930b919a50d2eacec361bff3
GCorreia3 <2218@rgsg.co.uk>
2023-03-01 17:25:25 +0000

    Celestial body colliding

--- celestial_body.py
+++ celestial_body.py
@@ -22,14 +22,30 @@ class CelestialBody():
     
     def attract(self):
         accelerations = []
+        objects_to_remove = []
+        object_to_add = []
         for object in game.OBJECTS:
             if object != self:
-                force = game.G * (self.mass * object.mass / (game.get_dist(self.x, self.y, object.x, object.y))**2)
-                direction = (object.x - self.x, object.y - self.y)
-                direction_magnitude = (direction[0]**2 + direction[1]**2)**0.5
-                force_direction = (direction[0] * (force / direction_magnitude), direction[1] * (force / direction_magnitude))
-                accelerations.append((force_direction[0] / self.mass, force_direction[1] / self.mass))
+                distance = game.get_dist(self.x, self.y, object.x, object.y)
 
+                if distance < self.radius:
+                    combined_mass = self.mass + object.mass
+                    object_to_add.append(CelestialBody(self.x, self.y, ((self.mass*self.velocity[0] + object.mass*object.velocity[0]) / combined_mass, (self.mass*self.velocity[1] + object.mass*object.velocity[1]) / combined_mass), combined_mass, self.radius + (object.radius / 2)))
+                    objects_to_remove.append(self)
+                    objects_to_remove.append(object)
+                else:
+                    force = game.G * (self.mass * object.mass / (distance)**2)
+                    direction = (object.x - self.x, object.y - self.y)
+                    direction_magnitude = (direction[0]**2 + direction[1]**2)**0.5
+                    force_direction = (direction[0] * (force / direction_magnitude), direction[1] * (force / direction_magnitude))
+                    accelerations.append((force_direction[0] / self.mass, force_direction[1] / self.mass))
+
+        for object in object_to_add:
+            game.OBJECTS.append(object)
+
+        for object in objects_to_remove:
+            game.OBJECTS.remove(object)
+        
         x = 0
         y = 0
         for i in range(len(accelerations)):
--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import sys
 import game
 from celestial_body import CelestialBody
 from time import perf_counter
+import random
 
 pygame.init()
-
-game.OBJECTS.append(CelestialBody(game.WIDTH / 4, game.HEIGHT/2, (0, 0.2), 10, 20))
-game.OBJECTS.append(CelestialBody(game.WIDTH / 2, game.HEIGHT/2, (0, 0), 1000, 30))
 
 running = True
 paused = True
@@ -22,6 +20,10 @@ def draw_screen(delta_time):
         object.draw()
 
     pygame.display.update()
+
+def mouse_down(mouse):
+    x, y = mouse
+    game.OBJECTS.append(CelestialBody(x, y, (random.random() * random.choice((-1, 1)) * 0.5, random.random() * random.choice((-1, 1)) * 0.5), 100, 20))
 
 def quit():
     pygame.quit()
@@ -44,6 +46,10 @@ while running:
 
         elif event.type == pygame.QUIT:
             quit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse = pygame.mouse.get_pos()
+            mouse_down(mouse)
 
     end_time = perf_counter()
     delta_time = end_time - start_time
